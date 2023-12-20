57df2c3d02bd04dd81d6f0c89331c8aa42741538
shania873 <vanaeca@hotmail.com>
2023-12-20 10:22:23 +0100

    basename added

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter basename={import.meta.env.DEV ? "/" : "/App-SportyApp-React"}>
+  <BrowserRouter
+    basename={process.env.PUBLIC_URL ? "/" : "/App-SportyApp-React"}
+  >
     <App />
   </BrowserRouter>,
   document.getElementById("root")
