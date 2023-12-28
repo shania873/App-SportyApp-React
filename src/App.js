612dd2b7836b107608022d1331adb3880e6aecca
shania873 <vanaeca@hotmail.com>
2023-12-28 17:14:29 +0100

    remove layout

--- src/App.js
+++ src/App.js
@@ -13,18 +13,18 @@ import ListMyLessons from "./components/MyLessons/List-MyLessons";
 function App() {
   return (
     <Routes>
-      <Route path={`/`} element={<Layout />}>
-        <Route path="unauthorized" element={<h1>Unauthorized</h1>} />
-        <Route
-          element={
-            <RequireAuth roles={["ROLE_ADMIN", "ROLE_BASIC", "ROLE_USER"]} />
-          }
-        >
-          <Route path={`/Login`} element={<Login />} exact />
-          <Route path={`/Home`} element={<Home />} exact />
-          <Route path={`/ListeDeCours`} element={<ListMyLessons />} />
-        </Route>
+      {/* <Route element={<Layout />}> */}
+      <Route path="unauthorized" element={<h1>Unauthorized</h1>} />
+      <Route
+        element={
+          <RequireAuth roles={["ROLE_ADMIN", "ROLE_BASIC", "ROLE_USER"]} />
+        }
+      >
+        <Route path={`/Login`} element={<Login />} exact />
+        <Route path={`/Home`} element={<Home />} exact />
+        <Route path={`/ListeDeCours`} element={<ListMyLessons />} />
       </Route>
+      {/* </Route> */}
     </Routes>
   );
 }
