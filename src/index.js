df295d0eee3680f079bdae2ee426ad1068a0528d
shania873 <vanaeca@hotmail.com>
2023-12-29 00:23:01 +0100

    add navigate

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ import { React, StrictMode } from "react";
 import ReactDOM from "react-dom";
 // import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider";
 
 console.log(process.env.PUBLIC_URL, "fgdfghhjkhjkhjkdfgh");
 ReactDOM.render(
   <AuthProvider>
-    <BrowserRouter basename={"/App-SportyApp-React"}>
+    <HashRouter basename="/App-SportyApp-React" hashType="hashbang">
       <StrictMode>
         <App />
       </StrictMode>
-    </BrowserRouter>
+    </HashRouter>
+    {/* <BrowserRouter basename={"/App-SportyApp-React"}>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </BrowserRouter> */}
   </AuthProvider>,
   document.getElementById("root")
 );
